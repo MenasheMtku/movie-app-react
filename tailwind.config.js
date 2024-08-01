e1448bf43cf15627f4b1b7e1286fafc003ae349e
root <root@menashe>
2024-08-01 08:40:24 +0300

    fix: fixed tailwind config file configuration by adding darkmode: 'class'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
-import withMT from "@material-tailwind/react/utils/withMT";
+// import withMT from "@material-tailwind/react/utils/withMT";
+// /** @type {import('tailwindcss').Config} */
+// export default withMT({
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     className: "dark",
+//     // darkMode: "selector",
+
+//     extend: {},
+//   },
+
+//   plugins: [],
+// });
 /** @type {import('tailwindcss').Config} */
-export default withMT({
+export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    className: "dark",
     extend: {},
   },
 
   plugins: [],
-});
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//     theme: {
-//         extend: {},
-//     },
-
-//     plugins: [],
-// }
+};
